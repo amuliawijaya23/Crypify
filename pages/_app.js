be21bf8fce1cb1ee5cafdb5e08ba75e0f3119693
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-08 22:50:52 +0000

    sets up redux store and provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import { Provider } from 'react-redux';
+import { wrapper, store } from '../state/store';
+
+// import from MUI
+import { Box, CssBaseline } from '@mui/material';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Box sx={{display: 'flex'}}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </Box>
+    </Provider>
+    )
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
