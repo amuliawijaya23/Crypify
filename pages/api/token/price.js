d2a4dd3e0b54eeed304b3ba4c5b778f01743b444
amuliawijaya23 <amuliawijaya@gmail.com>
2022-12-21 22:39:45 -0800

    adds imports from uniswap sdk

--- pages/api/token/price.js
+++ pages/api/token/price.js
@@ -1,6 +1,9 @@
 // import puppeteer
 import puppeteer from 'puppeteer';
 
+// import uniswap sdk
+import { ChainId, Token, Fetcher, Route, TradeType, TokenAmount, Trade } from '@uniswap/sdk'
+
 // Common Base Addresses
 const WETH = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
 const USDC = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
