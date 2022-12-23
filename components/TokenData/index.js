a0aff10d12d3e43243cdd9f08244a67e934c71e3
amuliawijaya23 <amuliawijaya@gmail.com>
2022-12-22 22:56:45 -0800

    formatting price

--- components/TokenData/index.js
+++ components/TokenData/index.js
@@ -23,17 +23,51 @@ const TokenData = () => {
   // global state
   const pool = useSelector((state) => state.pool.value);
 
+  const formatPrice = (price) => {
+    // const priceInString = String(pool?.profile?.token0?.price);
+    console.log('price', Number(price));
+    const priceInString = String(price);
+    const wholeNumber = priceInString.split('.')[0];
+    const decimals = priceInString.split('.')[1];
+
+    if (decimals) {
+      console.log('decimals', decimals)
+      let numberOfZero = 0;
+      let i = 0;
+      let trail = '';
+
+      while (decimals[i] === '0') {
+        numberOfZero++;
+        i++;
+      };
+
+      // if (numberOfZero > 3) {
+      //   return (
+      //     <span>{`$ ${wholeNumber}`}</span>
+      //   );
+      // };
+
+      return numberOfZero;
+
+    };
+  };
+
+  console.log('test', formatPrice(0.00000000135006));
+
+
   return (
     <Card sx={{ width: '100%', height: '100%', p: 0.5 }}>
       <CardHeader
 				title={
         <b>
-          <NumericFormat 
+          {/* <NumericFormat 
             value={pool?.profile?.token0?.price}
             displayType='text'
             prefix='$ '
-          />
-        </b>}
+          /> */}
+
+        </b>
+        }
 				titleTypographyProps={{ variant: 'body1' }}
 			/>
       <Divider />
